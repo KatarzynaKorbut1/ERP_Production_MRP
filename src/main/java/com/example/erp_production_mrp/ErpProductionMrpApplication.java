319341bfc146d525d5ccc57ea86f2e3e9d0cc912
Katarzyna Korbut <katarzyna.korbut@outlook.com>
2023-08-20 12:20:58 +0200

    Project Initialisation

---
+++ src/main/java/com/example/erp_production_mrp/ErpProductionMrpApplication.java
@@ -0,0 +1,13 @@
+package com.example.erp_production_mrp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpProductionMrpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErpProductionMrpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/erp_production_mrp/ErpProductionMrpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.erp_production_mrp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpProductionMrpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
