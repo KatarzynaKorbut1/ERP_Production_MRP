53c8629701392c7ace5892441cc624394b8d8f22
Michał <michaldr1@op.pl>
2023-08-27 09:26:30 +0200

    PurchasingOrder pierwsza wersja

--- src/main/java/com/example/erp_production_mrp/controller/RestApiControllerPurchasingOrder.java
+++ src/main/java/com/example/erp_production_mrp/controller/RestApiControllerPurchasingOrder.java
@@ -2,16 +2,26 @@ package com.example.erp_production_mrp.controller;
 
 import com.example.erp_production_mrp.model.PurchasingOrder;
 import com.example.erp_production_mrp.services.PurchasingOrderService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class RestApiControllerPurchasingOrder {
-    PurchasingOrderService purchasingOrderService;
+    private final PurchasingOrderService purchasingOrderService;  //pole musi być finalne jak chcemy wstrzyknac przez konstruktor
 
     public RestApiControllerPurchasingOrder(PurchasingOrderService purchasingOrderService) {
         this.purchasingOrderService = purchasingOrderService;
     }
+@PostMapping("/purchasing_order")
+public ResponseEntity<PurchasingOrder> createPurchasingOrder(@RequestBody PurchasingOrder purchasingOrder){
+        purchasingOrderService.addPurchasingOrderService(purchasingOrder);
+        return new ResponseEntity<>(purchasingOrder, HttpStatus.CREATED);
+}
+@GetMapping("/purchasing_order/{id}")
+    public ResponseEntity<PurchasingOrder> getPurchasingOrder(@PathVariable Long id){
+        return new ResponseEntity<>(purchasingOrderService.getPurchasingOrderById(id), HttpStatus.OK);
+}
 }
 
--- src/main/java/com/example/erp_production_mrp/services/PurchasingOrderService.java
+++ src/main/java/com/example/erp_production_mrp/services/PurchasingOrderService.java
@@ -3,6 +3,9 @@ package com.example.erp_production_mrp.services;
 import com.example.erp_production_mrp.model.PurchasingOrder;
 import com.example.erp_production_mrp.repositories.PurchasingOrderRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class PurchasingOrderService {
@@ -11,11 +14,15 @@ public class PurchasingOrderService {
     public PurchasingOrderService(PurchasingOrderRepository purchasingOrderRepository) {
         this.purchasingOrderRepository = purchasingOrderRepository;
     }
-    public void createPurchasingOrderService(PurchasingOrder purchasingOrder){
+@Transactional
+    public void addPurchasingOrderService(PurchasingOrder purchasingOrder){
         purchasingOrderRepository.save(purchasingOrder);
     }
-    public PurchasingOrder getPurchasingOrder(Long id){
+    public PurchasingOrder getPurchasingOrderById(Long id){
         return purchasingOrderRepository.findById(id).orElse(null);
+    }
+    public List<PurchasingOrder> getAll(){
+        return purchasingOrderRepository.findAll();
     }
 
 }
